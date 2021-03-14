90e023f2e6d6033000f72f96444eda9025d9c709
Satnam Singh <rssatnam@gmail.com>
2021-03-15 03:02:54 +0530

    Generate Horoscope Messages

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 // data
 const horoscope = {
-    signInfo: ["star", "moon", "sun", "comet"],
-    fortuneOutput: [
-      "terrible luck",
-      "bad luck",
-      "ok luck",
-      "good luck",
-      "great luck",
-    ],
-    advice: ["go out and eat", "not read this", "play more", "trust no one"],
-  };
-  
-  console.log("Testing");
-  
-  /*
-  - Output
-  Your sign is sun.
-  
-  You are having good luck. 
-  
-  You should: "trust no one"
-  */
-  
+  signInfo: ["star", "moon", "sun", "comet"],
+  fortuneOutput: [
+    "terrible luck",
+    "bad luck",
+    "ok luck",
+    "good luck",
+    "great luck",
+  ],
+  advice: ["go out and eat", "not read this", "play more", "trust no one"],
+};
+
+// random selection from array
+const randomSelection = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const generateHoroscope = () => {
+  let strArray = [];
+
+  for (const info in horoscope) {
+    // console.log(info);
+    let itemSelection = randomSelection(horoscope[info]);
+    switch (info) {
+      case "signInfo":
+        strArray.push(`Your sign is ${itemSelection}.`);
+        break;
+      case "fortuneOutput":
+        strArray.push(`You are having ${itemSelection}.`);
+        break;
+      case "advice":
+        strArray.push(`Your sign is ${itemSelection}.`);
+        break;
+      default:
+        console.log(`Something went wrong!`);
+        break;
+    }
+  }
+
+  console.log(strArray.join("\n\r"));
+};
+
+generateHoroscope();
