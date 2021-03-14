e4897b3db79419e26ecb72b4797ef3980fd9e092
Satnam Singh <rssatnam@gmail.com>
2021-03-14 22:46:32 +0530

    Horoscope Data Store

---
+++ script.js
@@ -0,0 +1,24 @@
+// data
+const horoscope = {
+    signInfo: ["star", "moon", "sun", "comet"],
+    fortuneOutput: [
+      "terrible luck",
+      "bad luck",
+      "ok luck",
+      "good luck",
+      "great luck",
+    ],
+    advice: ["go out and eat", "not read this", "play more", "trust no one"],
+  };
+  
+  console.log("Testing");
+  
+  /*
+  - Output
+  Your sign is sun.
+  
+  You are having good luck. 
+  
+  You should: "trust no one"
+  */
+  
